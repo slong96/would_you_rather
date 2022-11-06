11d7e864376e652b0510855d344303c2f8e433fe
slong96 <94485068+slong96@users.noreply.github.com>
2022-11-06 15:02:44 -0600

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,87 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app"> 
+
+    <h1>Welcome to Would You Rather</h1>
+    <!-- -->
+    <!-- v-on:answer-changed - calls "answerChanged" method when answer-change event happens -->
+    <WouldYouRather v-for="wyrQuestion in questions" v-bind:key="wyrQuestion"
+                    v-bind:questionsData="wyrQuestion"
+                    v-on:answer-changed="answerChanged">
+    </WouldYouRather>
+
+    <h4>You would rather be a...</h4>
+    <ul>
+      <li v-for="answer1 in answerList1" v-bind:key="answer1"> {{answer1}} </li>
+      <li v-for="answer2 in answerList2" v-bind:key="answer2"> {{answer2}} </li>
+      <li v-for="answer3 in answerList3" v-bind:key="answer3"> {{answer3}} </li>
+    </ul>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+  data() {
+    return {
+      questions: [
+        {
+          id: 0,
+          question: 'be a famous inventor or a famous writer?',
+          answer1: 'Famous Inventor',
+          answer2: 'Famous Writer'
+        },
+        {
+          id: 1,
+          question: 'be a wizard or a superhero?',
+          answer1: 'Wizard',
+          answer2: 'Superhero',
+        },
+        {
+          id: 2,
+          question: 'be a detective or a pilot?',
+          answer1: 'Detective',
+          answer2: 'Pilot',
+        }
+      ],
+      // array list for each question answers
+      answerList1: [],
+      answerList2: [],
+      answerList3: []
+    }
+  },
+  methods: {
+    answerChanged(answerSelected) {
+      // push answers to array list for question 1
+      if (answerSelected == this.questions[0].answer1) {
+        this.answerList1.pop(answerSelected)
+        this.answerList1.push(answerSelected)
+      } else if (answerSelected == this.questions[0].answer2) {
+        this.answerList1.pop(answerSelected)
+        this.answerList1.push(answerSelected)
+      }
+      // push answers to array list for question 2
+      if (answerSelected == this.questions[1].answer1) {
+        this.answerList2.pop(answerSelected)
+        this.answerList2.push(answerSelected)
+      } else if (answerSelected === this.questions[1].answer2) {
+        this.answerList2.pop(answerSelected)
+        this.answerList2.push(answerSelected)
+      }
+      // push answers to array list for question 3
+      if (answerSelected == this.questions[2].answer1) {
+        this.answerList3.pop(answerSelected)
+        this.answerList3.push(answerSelected)
+      } else if (answerSelected == this.questions[2].answer2) {
+        this.answerList3.pop(answerSelected)
+        this.answerList3.push(answerSelected)
+      }
+    }
   }
 }
 </script>
@@ -20,7 +92,15 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000000;
   margin-top: 60px;
+}
+
+#app li {
+  list-style: none;
+  font-size: 24px;
+  font-weight: bold;
+  background: yellow;
+  margin: 5px;
 }
 </style>
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="wyr">
+
+    <h2>Would you rather...</h2>
+
+    <!-- question from object array -->
+    <h3>{{ questionsData.question }}</h3>
+
+    <!-- v-on:change to call method (choiceMade) -->
+    <input type="radio" v-model="answerSelected" 
+                        v-bind:value="questionsData.answer1" 
+                        v-on:change="choiceMade"> 
+    
+    <!-- display answer1 from object array -->
+    <label>{{ questionsData.answer1 }}</label>
+    
+    <!-- v-on:change to call method (choiceMade) -->
+    <input type="radio" v-model="answerSelected" 
+                        v-bind:value="questionsData.answer2" 
+                        v-on:change="choiceMade"> 
+    
+    <!-- display answer2 from object array -->
+    <label>{{ questionsData.answer2 }}</label>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WouldYouRather',
+  props: {
+    questionsData: Object,
+  },
+  data() {
+    return {
+      answerSelected: ''
+    }
+  },
+  methods: {
+    choiceMade() {
+      // emit an event called 'answer-changed' and inculde the vale of the data "choice"
+      this.$emit('answer-changed', this.answerSelected)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+.wyr {
+  border: 1px rgb(0, 0, 0) solid;
+  background: rgb(255, 205, 205);
+  margin: 5px;
+}
+
+</style>
